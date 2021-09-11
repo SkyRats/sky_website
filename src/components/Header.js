951fc61bf5a827756621ba5447cf734c916759f0
Lucas Souza <outrolucas2002@gmail.com>
2021-09-10 22:50:37 -0300

    dropdown disabled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,42 @@
 import Dropdown from "./Dropdown";
 
+const pages = [
+  {
+    name: "Quem somos",
+    link: "#quem-somos",
+  },
+  {
+    name: "A Equipe",
+    link: "#equipe",
+  },
+  {
+    name: "Conquistas",
+    link: "#conquistas",
+  },
+  {
+    name: "Projetos",
+    link: "#projetos",
+  },
+  {
+    name: "Parcerias",
+    link: "#parcerias",
+  },
+  {
+    name: "Apoia-se",
+    link: "#apoia-se",
+  },
+];
+
 export default function Header() {
   return (
-    <div className="flex flex-row justify-between">
-      <span className='text-4xl'>SkyRatos</span> <Dropdown />
+    <div className="flex flex-row justify-between items-center">
+      <span className="text-4xl">SkyRatos</span>
+
+      <div className="flex flex-row">
+        {pages.map((item) => (
+          <a href={item.link} className='mx-3'> {item.name} </a>
+        ))}
+      </div>
     </div>
   );
 }
