b21071cb83575e52b7285843fb0a786988666b83
GabiYuri <62964262+GabiYuri@users.noreply.github.com>
2021-10-27 09:53:13 -0300

    conquistasMais

---
+++ src/components/ConquistasMais.js
@@ -0,0 +1,77 @@
+export default function ConquistasMais() {
+    return (
+      <div>
+        <div className="flex flex-row items-center">
+          <div>
+            <h1 className="text-3xl pb-5" id="conquistas">
+              Cobruf Drones 2018
+            </h1>
+            <p className="text-lg">
+              A primeira competição da qual a Skyrats participou foi a COBRUF 2018 
+              (Competição Brasileira Universitária de Foguetes), iniciativa independente 
+              de estudantes de graduação que promove e incentiva grupos técnicos-estudantis 
+              de diversas áreas como as voltadas para foguetes, pequenos satélites, trajes 
+              espaciais e drones. Nossa equipe atuou na categoria de drones, na qual o 
+              objetivo era desenvolver drones com foco em exploração espacial. Conseguimos 
+              conquistar o primeiro lugar nessa competição.
+            </p>
+
+            <h1 className="text-3xl pb-5" id="conquistas">
+              IMAV 2019
+            </h1>
+            <p className="text-lg">
+              A IMAV (International Micro Air Vehicle Conference and Competition), é uma 
+              competição que tem como propósito, além de avaliar as equipes participantes, 
+              estimular o desenvolvimento tecnológico, promovendo a realização de uma 
+              conferência acadêmica junto à competição. As provas são divididas em missões 
+              indoor (em ambientes fechados) e outdoor (em ambientes abertos), envolvem 
+              tarefas a serem realizadas autonomamente por drones e, em 2019, foram voltadas 
+              para o contexto de entrega e localização de pacotes, com missões que incluíam:
+              - a leitura de QR Codes e de alfanuméricos
+              - transporte de caixas
+              - carregamento cooperativo, entre outros
+              Alcançamos o 5° lugar na categoria outdoor e o 6° lugar na categoria indoor.
+            </p>
+
+            <h1 className="text-3xl pb-5" id="conquistas">
+              Cobruf Drones 2019/2020
+            </h1>
+            <p className="text-lg">
+              A equipe participou pela segunda vez da competição COBRUF, a qual iniciou-se 
+              em 2019 e durou até o final de 2020, com atrasos por causa da pandemia de 
+              Covid-19 e cujo tema foi a exploração espacial. A Skyrats participou da 
+              categoria de desafio, na qual foi apresentado um paper técnico sobre o 
+              desenvolvimento de tecnologias de drones para exploração espacial. Além disso, 
+              participamos, também, da categoria de drones, na qual o objetivo era projetar 
+              um drone para uma missão espacial a partir de uma série de requisitos como 
+              peso, capacidade de carga e capacidade de coleta de amostras. Levamos o 
+              primeiro lugar nessa edição.
+            </p>
+
+            <h1 className="text-3xl pb-5" id="conquistas">
+              CBR 2021
+            </h1>
+            <p className="text-lg">
+              A competição foi inteiramente virtual, realizada em uma simulação. A ideia 
+              era realizar missões em uma base petroleira, com bases tanto em terra, em barcos 
+              flutuantes ou em uma platarforma londe da costa. A competição foi composta por 
+              4 fases que exigiam mapeamento do local, pouso de precisão, análise de um tubo 
+              de transporte de gás, leitura de displays e de código QR e transporte de pacotes.
+              Foi a primeira vez que a equipe participou dessa conquista, levando primeiro lugar
+              como conquista.
+            </p>
+          </div>
+  
+          <div>
+            <img src="/images/equipe_fotos.png" alt="fotos das conquistas" />
+          
+            <img src="/images/equipe_fotos.png" alt="fotos das conquistas" />
+         
+            <img src="/images/equipe_fotos.png" alt="fotos das conquistas" />
+          
+            <img src="/images/equipe_fotos.png" alt="fotos das conquistas" />
+          </div>
+        </div>
+      </div>
+    );
+  }
