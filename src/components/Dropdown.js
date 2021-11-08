a541fb2111b966ec24f9ec6970a03def01944325
Lucas Souza <outrolucas2002@gmail.com>
2021-11-08 20:04:33 -0300

    fix props in dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,7 @@
 import { MenuIcon } from "@heroicons/react/solid";
 import { Menu, Transition } from "@headlessui/react";
 
-export default function Dropdown(props) {
+export default function Dropdown({pages}) {
   return (
     <Menu as="div" className="relative inline-block text-right">
       <Menu.Button className="bg-gray-dark bg-opacity-60 hover:bg-opacity-80 p-1 rounded-lg shadow-lg">
@@ -17,11 +17,12 @@ export default function Dropdown(props) {
         leaveTo="transform scale-95 opacity-0"
       >
         <Menu.Items className="absolute right-0 w-56 mt-1 origin-top-right bg-gray-dark bg-opacity-80 rounded-lg shadow-lg flex flex-col p-1">
-          {props.pages.map((item) => (
+          {pages.map((item) => (
             <Menu.Item>
               <a
                 href={item.link}
                 className="rounded-md w-full p-2 text-lg hover:bg-gray-dark"
+                key={item.name}
               >
                 {" "}
                 {item.name}{" "}
