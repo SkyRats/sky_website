2251300fd6c6c7cb878d2b3fbdf714722ce7d894
Brenda Moreira <bremsantos@outlook.com>
2022-04-15 15:39:49 -0300

    alterações carômetro

--- src/components/CarometroButton.js
+++ src/components/CarometroButton.js
@@ -3,11 +3,14 @@ export default function CarometroButton({src, name, ...props}) {
       <button className="flex flex-col items-center transition duration-300 ease-in-out hover:scale-110"
               {...props}
       >
+        <div class="hexagon">
         <img
-          src={"/images/carometro_img/" + src + ".jpg"}
+          src={"/images/carometro_img/" + src + ".png"}
           alt={"icone " + name}
-          className="h-24 w-auto object-cover rounded-full"
+          className="h-24 w-auto object-cover"
         />
+        </div>
+        
         <h1 className="text-sm md:text-lg">{name}</h1>
       </button>
     );
