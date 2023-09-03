794f087144560eed0bd031b094d21e1bfc8ca119
Felipe Beserra <felipebeserra25@gmail.com>
2023-09-02 22:21:19 -0300

    Added redirect for QrCode

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/greet',
+        destination: 'https://www.instagram.com/skyrats_drones/',
+        permanent: false
+      }
+    ];
+  }
 }
