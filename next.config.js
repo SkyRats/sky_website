e71b365545f1c0b55895b28efba6eabd8031ba9d
Skyrats <93954688+SkyratsDrones@users.noreply.github.com>
2024-09-11 15:04:20 -0300

    Future proof URLs

--- next.config.js
+++ next.config.js
@@ -9,23 +9,23 @@ module.exports = {
         permanent: false
       },
       {
-        source: '/pilot/vilas',
-        destination: 'https://docs.google.com/spreadsheets/d/1hUbA3VqjouE7MHPKiWJByQ7fNmMT3xUGOw4txx6Tbew/edit?gid=0#gid=0',
+        source: '/pilot/outdoor/0',
+        destination: 'https://docs.google.com/spreadsheets/d/1hUbA3VqjouE7MHPKiWJByQ7fNmMT3xUGOw4txx6Tbew/edit?gid=1166893714#gid=1166893714',
         permanent: false
       },
       {
-        source: '/pilot/beserra',
-        destination: 'https://docs.google.com/spreadsheets/d/1hUbA3VqjouE7MHPKiWJByQ7fNmMT3xUGOw4txx6Tbew/edit?gid=653802211#gid=653802211',
+        source: '/pilot/outdoor/1',
+        destination: 'https://docs.google.com/spreadsheets/d/1hUbA3VqjouE7MHPKiWJByQ7fNmMT3xUGOw4txx6Tbew/edit?gid=315769465#gid=315769465',
         permanent: false
       },
       {
-        source: '/pilot/cassim',
+        source: '/pilot/indoor/0',
         destination: 'https://docs.google.com/spreadsheets/d/1hUbA3VqjouE7MHPKiWJByQ7fNmMT3xUGOw4txx6Tbew/edit?gid=839429704#gid=839429704',
         permanent: false
       },
       {
-        source: '/pilot/marcos',
-        destination: 'https://docs.google.com/spreadsheets/d/1hUbA3VqjouE7MHPKiWJByQ7fNmMT3xUGOw4txx6Tbew/edit?gid=1166893714#gid=1166893714',
+        source: '/pilot/indoor/1',
+        destination: 'https://docs.google.com/spreadsheets/d/1hUbA3VqjouE7MHPKiWJByQ7fNmMT3xUGOw4txx6Tbew/edit?gid=653802211#gid=653802211',
         permanent: false
       }
     ];
